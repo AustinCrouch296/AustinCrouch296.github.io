c76bcee55a4b7cff5b63c374249676ab29ad5366
Austin <AustinCrouch296@gmail.com>
2022-10-30 11:58:04 -0400

    fixed error on vaporwave page

--- assets/js/load-flickity.js
+++ assets/js/load-flickity.js
@@ -4,13 +4,3 @@ var $carousel = $('.main-carousel').flickity({
   cellAlign: "center",
   imagesLoaded: "true"
 });
-
-var caption_to_change = document.getElementById("flickity-caption");
-// Flickity instance
-var flkty = $carousel.data('flickity');
-
-$carousel.on( 'select.flickity', function() {
-  // set image caption using img's alt
-  var caption = $(flkty.selectedElement).find('img').attr('alt')
-  caption_to_change.innerHTML = caption;
-});
