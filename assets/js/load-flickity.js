77f719f5ef0aa5e8e56fc44535b1e96aa547092b
Austin <AustinCrouch296@gmail.com>
2022-11-06 17:53:23 -0500

    Flickity fullscreen viewer

--- assets/js/load-flickity.js
+++ assets/js/load-flickity.js
@@ -2,5 +2,18 @@ var $carousel = $('.main-carousel').flickity({
   // options
   wrapAround: "true", 
   cellAlign: "center",
-  imagesLoaded: "true"
+  imagesLoaded: "true",
 });
+
+function fsFlickity() {
+  var carousel = {};
+  carousel = document.getElementById("main-carousel");
+  btn = document.getElementById("flickity-fullscreen-btn");
+
+  carousel.classList.toggle('is-fullscreen');
+  btn.classList.toggle('active');
+  $carousel.flickity('resize');
+
+  setTimeout(function() {
+    $carousel.flickity('resize');}, 500);
+}
