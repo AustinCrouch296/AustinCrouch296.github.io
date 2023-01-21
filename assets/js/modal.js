91b10d1f031b1c6b8594ecdc8648b6f5723ce8db
Austin <AustinCrouch296@gmail.com>
2023-01-20 22:45:46 -0500

    Added models and model-viewer, better formatting

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,6 +2,8 @@
 var currentModal = {};
 var mv = {};
 var mv_btn = {};
+const modelviewers = document.getElementsByClassName("modelviewer");
+const modelviewer_captions = document.getElementsByClassName("modelviewer-caption");
 
 function openModal(modal_id) {
     // Get the modal element, and set style to display
@@ -40,6 +42,28 @@ function stopModel() {
     if (!isEmpty) {
         mv_btn.style.display = 'inline-block';
         mv.showPoster();
-        mv.cameraOrbit = '0deg 75deg 100%';
+
+        if (mv.id == "modelviewer1") {
+            mv.cameraOrbit = '-65deg 75deg 100%';
+        } 
+        else if (mv.id == "modelviewer2") {
+            mv.cameraOrbit = '35deg 70deg 100%';
+        }
+        else {
+            mv.cameraOrbit = '0deg 75deg 100%';
+        }
     }
+}
+
+function selectModelViewer(mv_id) {
+    stopModel();
+    for (let i = 0; i < modelviewers.length; i++) {
+        modelviewers[i].classList.add("hidden");
+        modelviewer_captions[i].classList.add("hidden");
+    }
+
+    selected_mv = document.getElementById("modelviewer" + mv_id);
+    selected_mv_caption = document.getElementById("modelviewer-caption" + mv_id);
+    selected_mv.classList.remove("hidden");
+    selected_mv_caption.classList.remove("hidden");
 }
